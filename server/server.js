1bfe490e02612a58f1b14fe9303a3b818ad40996
Shaurya Dwivedi <164564564+Shaurya068@users.noreply.github.com>
2026-02-07 01:33:34 +0530

    Configure CORS for specific origin and methods

--- server/server.js
+++ server/server.js
@@ -11,7 +11,12 @@ const PORT = process.env.PORT || 3000
 await connectDb();
 app.use(express.json())
 
-app.use(cors())
+app.use(cors({
+  origin: "https://resume-builder-woad-eta.vercel.app",
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true
+}));
+
 
 
 app.get('/', (req, res) => {
